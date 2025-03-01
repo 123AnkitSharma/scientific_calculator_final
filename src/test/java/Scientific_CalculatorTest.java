55b49a19328ccd31564aae0cceb389a296d21179
Ankit <84094757+123AnkitSharma@users.noreply.github.com>
2025-03-01 14:38:41 +0530

    Update Scientific_CalculatorTest.java

--- src/test/java/Scientific_CalculatorTest.java
+++ src/test/java/Scientific_CalculatorTest.java
@@ -5,7 +5,7 @@ public class Scientific_CalculatorTest {
 
     @Test
     public void testSquareRoot() {
-        assertEquals(5.0, Scientific_Calculator.squareRoot(25.0), 0.0001);
+        assertEquals(4.0, Scientific_Calculator.squareRoot(25.0), 0.0001);
         assertEquals(3.0, Scientific_Calculator.squareRoot(9.0), 0.0001);
     }
 
